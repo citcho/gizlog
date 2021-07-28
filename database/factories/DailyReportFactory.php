793f99d515dcc89434ad807794421e271223480c
kobayashi <kobayashi@gizumo-admin-017noMacBook-Pro.local>
2021-07-29 01:55:28 +0900

    fix: DailyReportSeeder 実行速度

--- database/factories/DailyReportFactory.php
+++ database/factories/DailyReportFactory.php
@@ -1,14 +1,13 @@
 <?php
 
 use App\Models\DailyReport;
-use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(DailyReport::class, function (Faker $faker) {
     return [
-        'user_id' => User::all()->random()->id,
+        'user_id' => $faker->numberBetween(1, 4),
         'reporting_time' => $faker->dateTimeBetween('-100day', 'now')->format('Y-m-d'),
         'title' => $faker->title,
-        'content' => $faker->sentence(rand(1,4)) . PHP_EOL . $faker->sentence(rand(1,4)),
+        'content' => $faker->sentence(rand(1, 4)) . PHP_EOL . $faker->sentence(rand(1, 4)),
     ];
 });
--- database/seeds/DailyReportsSeeder.php
+++ database/seeds/DailyReportsSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\DailyReport;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,6 +15,7 @@ class DailyReportsSeeder extends Seeder
     public function run()
     {
         DB::table('daily_reports')->truncate();
+
         factory(DailyReport::class, 100)->create();
     }
 }
