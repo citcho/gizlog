3e517f4669223bb98c6f68f5757298874ebc2a48
kobayashi <kobayashi@gizumo-admin-017noMacBook-Pro.local>
2021-08-16 01:51:17 +0900

    add: migration file books

---
+++ database/migrations/2021_08_13_081453_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title', 100);
+            $table->string('author', 100);
+            $table->string('publisher', 100);
+            $table->unsignedInteger('price');
+            $table->date('purchase_date');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
