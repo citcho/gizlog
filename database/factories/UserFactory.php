e3dd1a3cb96f77dc22e10b137a3d4430215ccb69
kobayashi <kobayashi@gizumo-admin-017noMacBook-Pro.local>
2021-10-11 18:38:33 +0900

    change: ダミーユーザーのアバター追加

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'slack_user_id' => $faker->asciify('**********'),
         'email' => $faker->unique()->safeEmail,
-        'avatar' => null,
+        'avatar' => 'https://i.pinimg.com/236x/18/64/f1/1864f1c531cf2ee82c7400bb298f4b2d.jpg',
         'remember_token' => str_random(10),
     ];
 });
