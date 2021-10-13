b378a10947f7ef2917856ed07c12769f5d832715
kobayashi <kobayashi@gizumo-admin-017noMacBook-Pro.local>
2021-10-13 19:01:27 +0900

    add: attendance factoryの追加

--- database/factories/AttendanceFactory.php
+++ database/factories/AttendanceFactory.php
@@ -4,10 +4,13 @@ use App\Models\Attendance;
 use Faker\Generator as Faker;
 
 $factory->define(Attendance::class, function (Faker $faker) {
+
     return [
         'user_id' => rand(1, 500),
-        'date' => $faker->date(),
-        'start_time' => time(),
-        'end_time' => time(),
+        'date' => date('Y-m-d'),
+        // 9:00~11:00
+        'start_time' => date('H:i:s', rand(0, 7200)),
+        // 18:00~20:00
+        'end_time' => date('H:i:s', rand(32400, 39600)),
     ];
 });
