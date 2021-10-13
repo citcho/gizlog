94422e76b69ef7898ad41fef69d81ec77e8ef88f
kobayashi <kobayashi@gizumo-admin-017noMacBook-Pro.local>
2021-10-13 19:02:24 +0900

    attendance seederの作成

---
+++ database/seeds/AttendanceTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\Attendance;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AttendanceTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('attendance')->truncate();
+        factory(Attendance::class, 100)->create();
+    }
+}
