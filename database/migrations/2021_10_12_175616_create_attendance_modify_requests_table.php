d01dd96236289376e2fdb797d1964760e5f2c62f
kobayashi <kobayashi@gizumo-admin-017noMacBook-Pro.local>
2021-10-15 12:28:35 +0900

    change: 修正依頼テーブル名変更

--- database/migrations/2021_10_12_175616_create_attendance_modify_requests_table.php
+++ database/migrations/2021_10_12_175616_create_attendance_modify_requests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAttendanceCorrectionRequestsTable extends Migration
+class CreateAttendanceModifyRequestsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAttendanceCorrectionRequestsTable extends Migration
      */
     public function up()
     {
-        Schema::create('attendance_correction_requests', function (Blueprint $table) {
+        Schema::create('attendance_modify_requests', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('attendance_id');
             $table->text('content');
@@ -28,6 +28,6 @@ class CreateAttendanceCorrectionRequestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('attendance_correction_requests');
+        Schema::dropIfExists('attendance_modify_requests');
     }
 }
