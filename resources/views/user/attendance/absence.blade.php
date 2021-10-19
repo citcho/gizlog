1c35b87e3368f901c79866296a888c03a9452a21
kobayashi <kobayashi@gizumo-admin-017noMacBook-Pro.local>
2021-10-19 19:11:38 +0900

    change: Laravel Collectiveの記法に変更

--- resources/views/user/attendance/absence.blade.php
+++ resources/views/user/attendance/absence.blade.php
@@ -4,12 +4,12 @@
 <h2 class="brand-header">欠席登録</h2>
 <div class="main-wrap">
   <div class="container">
-    <form>
+    {!! Form::open(['route' => 'attendance.absence']) !!}
       <div class="form-group">
-        <textarea class="form-control" placeholder="欠席理由を入力してください。" name="" cols="50" rows="10"></textarea>
+        {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => '欠席理由を入力してください。', 'cols' => 50, 'rows' => 10]) !!}
       </div>
-      <input name="confirm" class="btn btn-success pull-right" type="submit" value="登録">
-    </form>
+      {!! Form::submit('登録', ['class' => 'btn btn-success pull-right']) !!}
+    {!! Form::close() !!}
   </div>
 </div>
 
